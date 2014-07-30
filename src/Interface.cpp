79b3a7c2b35d161897222bff134e2546906490a0
jarrah-95 <jarrah-95@live.com.au>
2014-07-30 22:22:00 +1000

    Create Interface

---
+++ src/Interface.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   Interface.cpp
+ * Author: jarrah
+ *
+ * Created on 30 July 2014, 10:21 PM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+	// initialize resources, if needed
+	// Q_INIT_RESOURCE(resfile);
+
+	QApplication app(argc, argv);
+
+	// create and show your widgets here
+
+	return app.exec();
+}
